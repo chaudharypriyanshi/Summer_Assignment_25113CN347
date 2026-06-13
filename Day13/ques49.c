4d7043f3870c08f505905fa16214795bc0dc8d25
Priyanshi Chaudhary <chaudharypriyanshi993@gmail.com>
2026-06-13 21:39:42 +0530

    Day13 commit

---
+++ Day13/ques49.c
@@ -0,0 +1,24 @@
+//WAP to input and display array
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Array elements are:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
