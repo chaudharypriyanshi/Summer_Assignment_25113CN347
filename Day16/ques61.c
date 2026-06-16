706c899d4555af741bade505b0fb80f58b0cb0cb
Priyanshi Chaudhary <chaudharypriyanshi993@gmail.com>
2026-06-16 22:57:05 +0530

    Day 16 commit

---
+++ Day16/ques61.c
@@ -0,0 +1,24 @@
+//WAP to find missing number in array
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+    int sum = 0, total;
+
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n - 1);
+    for(i = 0; i < n - 1; i++)
+    {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+
+    total = n * (n + 1) / 2;
+
+    printf("Missing number = %d", total - sum);
+
+    return 0;
+}
---
+++ Day16/ques62.c
@@ -0,0 +1,36 @@
+//WAP to find max freq element
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, j;
+    int maxFreq = 0, element;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < n; i++)
+    {
+        int count = 1;
+
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[i] == a[j])
+                count++;
+        }
+
+        if(count > maxFreq)
+        {
+            maxFreq = count;
+            element = a[i];
+        }
+    }
+
+    printf("Maximum frequency element = %d", element);
+
+    return 0;
+}
